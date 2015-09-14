d46273bb884024ccd5687dea4a5c5f53b78446cc
keitharm <keith1015@gmail.com>
2015-09-14 16:10:29 -0700

    Fixed broken specs

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -13,62 +13,74 @@ var Link = require('../app/models/link');
 // Remove the 'x' from beforeEach block when working on
 // authentication tests.
 /************************************************************/
-var xbeforeEach = function(){};
+var xbeforeEach = function() {};
 /************************************************************/
 
+setTimeout(function() {
+  describe('', function() {
 
-describe('', function() {
+    beforeEach(function() {
+      // log out currently signed in user
+      request('http://127.0.0.1:4568/logout', function(error, res, body) {});
 
-  beforeEach(function() {
-    // log out currently signed in user
-    request('http://127.0.0.1:4568/logout', function(error, res, body) {});
+      // delete link for roflzoo from db so it can be created later for the test
+      db.knex('urls')
+        .where('url', '=', 'http://roflzoo.com/')
+        .del()
+        .catch(function(error) {
+          throw {
+            type: 'DatabaseError',
+            message: 'Failed to create test setup data'
+          };
+        });
 
-    // delete link for roflzoo from db so it can be created later for the test
-    db.knex('urls')
-      .where('url', '=', 'http://roflzoo.com/')
-      .del()
-      .catch(function(error) {
-        throw {
-          type: 'DatabaseError',
-          message: 'Failed to create test setup data'
-        };
+      // delete user Svnh from db so it can be created later for the test
+      db.knex('users')
+        .where('username', '=', 'Svnh')
+        .del()
+        .catch(function(error) {
+          // uncomment when writing authentication tests
+          // throw {
+          //   type: 'DatabaseError',
+          //   message: 'Failed to create test setup data'
+          // };
+        });
+
+      // delete user Phillip from db so it can be created later for the test
+      db.knex('users')
+        .where('username', '=', 'Phillip')
+        .del()
+        .catch(function(error) {
+          // uncomment when writing authentication tests
+          // throw {
+          //   type: 'DatabaseError',
+          //   message: 'Failed to create test setup data'
+          // };
+        });
+
+      // save a link to the database
+      link = new Link({
+        url: 'http://roflzoo.com/',
+        title: 'Funny pictures of animals, funny dog pictures',
+        base_url: 'http://127.0.0.1:4568'
       });
 
-    // delete user Svnh from db so it can be created later for the test
-    db.knex('users')
-      .where('username', '=', 'Svnh')
-      .del()
-      .catch(function(error) {
-        // uncomment when writing authentication tests
-        // throw {
-        //   type: 'DatabaseError',
-        //   message: 'Failed to create test setup data'
-        // };
+      link.save();
+    });
+
+    describe('Link creation:', function() {
+
+      var requestWithSession = request.defaults({
+        jar: true
       });
 
-    // delete user Phillip from db so it can be created later for the test
-    db.knex('users')
-      .where('username', '=', 'Phillip')
-      .del()
-      .catch(function(error) {
-        // uncomment when writing authentication tests
-        // throw {
-        //   type: 'DatabaseError',
-        //   message: 'Failed to create test setup data'
-        // };
-      });
-  });
+      var beforeEach = function() {};
 
-  describe('Link creation:', function(){
-
-    var requestWithSession = request.defaults({jar: true});
-
-var xbeforeEach = function(){};
       // create a user that we can then log-in with
       new User({
-          'username': 'Phillip',
-          'password': 'Phillip'
-      }).save().then(function(){
+        'username': 'Phillip',
+        'password': 'Phillip'
+      }).save().then(function() {
         var options = {
           'method': 'POST',
           'followAllRedirects': true,
@@ -83,90 +95,25 @@ var xbeforeEach = function(){};
           done();
         });
       });
-    });
 
-    it('Only shortens valid urls, returning a 404 - Not found for invalid urls', function(done) {
-      var options = {
-        'method': 'POST',
-        'uri': 'http://127.0.0.1:4568/links',
-        'json': {
-          'url': 'definitely not a valid url'
-        }
-      };
+      it('Only shortens valid urls, returning a 404 - Not found for invalid urls', function(done) {
+        var options = {
+          'method': 'POST',
+          'uri': 'http://127.0.0.1:4568/links',
+          'json': {
+            'url': 'definitely not a valid url'
+          }
+        };
 
-      requestWithSession(options, function(error, res, body) {
-        // res comes from the request module, and may not follow express conventions
-        expect(res.statusCode).to.equal(404);
-        done();
-      });
-    });
-
-    describe('Shortening links:', function(){
-
-      var options = {
-        'method': 'POST',
-        'followAllRedirects': true,
-        'uri': 'http://127.0.0.1:4568/links',
-        'json': {
-          'url': 'http://roflzoo.com/'
-        }
-      };
-
-      it('Responds with the short code', function(done) {
         requestWithSession(options, function(error, res, body) {
-          expect(res.body.url).to.equal('http://roflzoo.com/');
-          expect(res.body.code).to.not.be.null;
+          // res comes from the request module, and may not follow express conventions
+          expect(res.statusCode).to.equal(404);
           done();
         });
       });
 
-      it('New links create a database entry', function(done) {
-        requestWithSession(options, function(error, res, body) {
-          db.knex('urls')
-            .where('url', '=', 'http://roflzoo.com/')
-            .then(function(urls) {
-              if (urls['0'] && urls['0']['url']) {
-                var foundUrl = urls['0']['url'];
-              }
-              expect(foundUrl).to.equal('http://roflzoo.com/');
-              done();
-            });
-        });
-      });
+      describe('Shortening links:', function() {
 
-      it('Fetches the link url title', function (done) {
-        requestWithSession(options, function(error, res, body) {
-          db.knex('urls')
-            .where('title', '=', 'Funny pictures of animals, funny dog pictures')
-            .then(function(urls) {
-              if (urls['0'] && urls['0']['title']) {
-                var foundTitle = urls['0']['title'];
-              }
-              expect(foundTitle).to.equal('Funny pictures of animals, funny dog pictures');
-              done();
-            });
-        });
-      });
-
-    }); // 'Shortening links'
-
-    describe('With previously saved urls:', function(){
-
-      var link;
-
-      beforeEach(function(done){
-        // save a link to the database
-        link = new Link({
-          url: 'http://roflzoo.com/',
-          title: 'Funny pictures of animals, funny dog pictures',
-          base_url: 'http://127.0.0.1:4568'
-        });
-        link.save().then(function(){
-          done();
-        });
-      });
-
-      it('Returns the same shortened code', function(done) {
         var options = {
           'method': 'POST',
           'followAllRedirects': true,
@@ -176,161 +123,215 @@ var xbeforeEach = function(){};
           }
         };
 
-        requestWithSession(options, function(error, res, body) {
-          var code = res.body.code;
-          expect(code).to.equal(link.get('code'));
-          done();
-        });
-      });
-
-      it('Shortcode redirects to correct url', function(done) {
-        var options = {
-          'method': 'GET',
-          'uri': 'http://127.0.0.1:4568/' + link.get('code')
-        };
-
-        requestWithSession(options, function(error, res, body) {
-          var currentLocation = res.request.href;
-          expect(currentLocation).to.equal('http://roflzoo.com/');
-          done();
-        });
-      });
-
-      it('Returns all of the links to display on the links page', function(done) {
-        var options = {
-          'method': 'GET',
-          'uri': 'http://127.0.0.1:4568/links'
-        };
-
-        requestWithSession(options, function(error, res, body) {
-          expect(body).to.include('"title":"Funny pictures of animals, funny dog pictures"');
-          expect(body).to.include('"code":"' + link.get('code') + '"');
-          done();
-        });
-      });
-
-    }); // 'With previously saved urls'
-
-  }); // 'Link creation'
-
-  xdescribe('Privileged Access:', function(){
-
-    it('Redirects to login page if a user tries to access the main page and is not signed in', function(done) {
-      request('http://127.0.0.1:4568/', function(error, res, body) {
-        expect(res.req.path).to.equal('/login');
-        done();
-      });
-    });
-
-    it('Redirects to login page if a user tries to create a link and is not signed in', function(done) {
-      request('http://127.0.0.1:4568/create', function(error, res, body) {
-        expect(res.req.path).to.equal('/login');
-        done();
-      });
-    });
-
-    it('Redirects to login page if a user tries to see all of the links and is not signed in', function(done) {
-      request('http://127.0.0.1:4568/links', function(error, res, body) {
-        expect(res.req.path).to.equal('/login');
-        done();
-      });
-    });
-
-  }); // 'Priviledged Access'
-
-  xdescribe('Account Creation:', function(){
-
-    it('Signup creates a user record', function(done) {
-      var options = {
-        'method': 'POST',
-        'uri': 'http://127.0.0.1:4568/signup',
-        'json': {
-          'username': 'Svnh',
-          'password': 'Svnh'
-        }
-      };
-
-      request(options, function(error, res, body) {
-        db.knex('users')
-          .where('username', '=', 'Svnh')
-          .then(function(res) {
-            if (res[0] && res[0]['username']) {
-              var user = res[0]['username'];
-            }
-            expect(user).to.equal('Svnh');
+        it('Responds with the short code', function(done) {
+          requestWithSession(options, function(error, res, body) {
+            expect(res.body.url).to.equal('http://roflzoo.com/');
+            expect(res.body.code).to.not.be.null;
             done();
-          }).catch(function(err) {
-            throw {
-              type: 'DatabaseError',
-              message: 'Failed to create test setup data'
-            };
           });
-      });
-    });
+        });
 
-    it('Signup logs in a new user', function(done) {
-      var options = {
-        'method': 'POST',
-        'uri': 'http://127.0.0.1:4568/signup',
-        'json': {
+        it('New links create a database entry', function(done) {
+          requestWithSession(options, function(error, res, body) {
+            db.knex('urls')
+              .where('url', '=', 'http://roflzoo.com/')
+              .then(function(urls) {
+                if (urls['0'] && urls['0']['url']) {
+                  var foundUrl = urls['0']['url'];
+                }
+                expect(foundUrl).to.equal('http://roflzoo.com/');
+                done();
+              });
+          });
+        });
+
+        it('Fetches the link url title', function(done) {
+          requestWithSession(options, function(error, res, body) {
+            db.knex('urls')
+              .where('title', '=', 'Funny pictures of animals, funny dog pictures')
+              .then(function(urls) {
+                if (urls['0'] && urls['0']['title']) {
+                  var foundTitle = urls['0']['title'];
+                }
+                expect(foundTitle).to.equal('Funny pictures of animals, funny dog pictures');
+                done();
+              });
+          });
+        });
+
+      }); // 'Shortening links'
+
+      describe('With previously saved urls:', function() {
+
+        it('Returns the same shortened code', function(done) {
+          var options = {
+            'method': 'POST',
+            'followAllRedirects': true,
+            'uri': 'http://127.0.0.1:4568/links',
+            'json': {
+              'url': 'http://roflzoo.com/'
+            }
+          };
+
+          requestWithSession(options, function(error, res, body) {
+            var code = res.body.code;
+            expect(code).to.equal(link.get('code'));
+            done();
+          });
+        });
+
+        it('Shortcode redirects to correct url', function(done) {
+          var options = {
+            'method': 'GET',
+            'uri': 'http://127.0.0.1:4568/' + link.get('code')
+          };
+
+          requestWithSession(options, function(error, res, body) {
+            var currentLocation = res.request.href;
+            expect(currentLocation).to.equal('http://roflzoo.com/');
+            done();
+          });
+        });
+
+        it('Returns all of the links to display on the links page', function(done) {
+          var options = {
+            'method': 'GET',
+            'uri': 'http://127.0.0.1:4568/links'
+          };
+
+          requestWithSession(options, function(error, res, body) {
+            expect(body).to.include('"title":"Funny pictures of animals, funny dog pictures"');
+            expect(body).to.include('"code":"' + link.get('code') + '"');
+            done();
+          });
+        });
+
+      }); // 'With previously saved urls'
+
+    }); // 'Link creation'
+
+    xdescribe('Privileged Access:', function() {
+
+      it('Redirects to login page if a user tries to access the main page and is not signed in', function(done) {
+        request('http://127.0.0.1:4568/', function(error, res, body) {
+          expect(res.req.path).to.equal('/login');
+          done();
+        });
+      });
+
+      it('Redirects to login page if a user tries to create a link and is not signed in', function(done) {
+        request('http://127.0.0.1:4568/create', function(error, res, body) {
+          expect(res.req.path).to.equal('/login');
+          done();
+        });
+      });
+
+      it('Redirects to login page if a user tries to see all of the links and is not signed in', function(done) {
+        request('http://127.0.0.1:4568/links', function(error, res, body) {
+          expect(res.req.path).to.equal('/login');
+          done();
+        });
+      });
+
+    }); // 'Priviledged Access'
+
+    xdescribe('Account Creation:', function() {
+
+      it('Signup creates a user record', function(done) {
+        var options = {
+          'method': 'POST',
+          'uri': 'http://127.0.0.1:4568/signup',
+          'json': {
+            'username': 'Svnh',
+            'password': 'Svnh'
+          }
+        };
+
+        request(options, function(error, res, body) {
+          db.knex('users')
+            .where('username', '=', 'Svnh')
+            .then(function(res) {
+              if (res[0] && res[0]['username']) {
+                var user = res[0]['username'];
+              }
+              expect(user).to.equal('Svnh');
+              done();
+            }).catch(function(err) {
+              throw {
+                type: 'DatabaseError',
+                message: 'Failed to create test setup data'
+              };
+            });
+        });
+      });
+
+      it('Signup logs in a new user', function(done) {
+        var options = {
+          'method': 'POST',
+          'uri': 'http://127.0.0.1:4568/signup',
+          'json': {
+            'username': 'Phillip',
+            'password': 'Phillip'
+          }
+        };
+
+        request(options, function(error, res, body) {
+          expect(res.headers.location).to.equal('/');
+          done();
+        });
+      });
+
+    }); // 'Account Creation'
+
+    xdescribe('Account Login:', function() {
+
+      var requestWithSession = request.defaults({
+        jar: true
+      });
+
+      beforeEach(function(done) {
+        new User({
           'username': 'Phillip',
           'password': 'Phillip'
-        }
-      };
+        }).save().then(function() {
+          done()
+        });
+      })
 
-      request(options, function(error, res, body) {
-        expect(res.headers.location).to.equal('/');
-        done();
+      it('Logs in existing users', function(done) {
+        var options = {
+          'method': 'POST',
+          'uri': 'http://127.0.0.1:4568/login',
+          'json': {
+            'username': 'Phillip',
+            'password': 'Phillip'
+          }
+        };
+
+        requestWithSession(options, function(error, res, body) {
+          expect(res.headers.location).to.equal('/');
+          done();
+        });
       });
-    });
 
-  }); // 'Account Creation'
+      it('Users that do not exist are kept on login page', function(done) {
+        var options = {
+          'method': 'POST',
+          'uri': 'http://127.0.0.1:4568/login',
+          'json': {
+            'username': 'Fred',
+            'password': 'Fred'
+          }
+        };
 
-  xdescribe('Account Login:', function(){
-
-    var requestWithSession = request.defaults({jar: true});
-
-    beforeEach(function(done){
-      new User({
-          'username': 'Phillip',
-          'password': 'Phillip'
-      }).save().then(function(){
-        done()
+        requestWithSession(options, function(error, res, body) {
+          expect(res.headers.location).to.equal('/login');
+          done();
+        });
       });
-    })
 
-    it('Logs in existing users', function(done) {
-      var options = {
-        'method': 'POST',
-        'uri': 'http://127.0.0.1:4568/login',
-        'json': {
-          'username': 'Phillip',
-          'password': 'Phillip'
-        }
-      };
+    }); // 'Account Login'
+  });
 
-      requestWithSession(options, function(error, res, body) {
-        expect(res.headers.location).to.equal('/');
-        done();
-      });
-    });
-
-    it('Users that do not exist are kept on login page', function(done) {
-      var options = {
-        'method': 'POST',
-        'uri': 'http://127.0.0.1:4568/login',
-        'json': {
-          'username': 'Fred',
-          'password': 'Fred'
-        }
-      };
-
-      requestWithSession(options, function(error, res, body) {
-        expect(res.headers.location).to.equal('/login');
-        done();
-      });
-    });
-
-  }); // 'Account Login'
-
-});
+  run();
+}, 1000);
